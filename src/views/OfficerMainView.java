d4a3695f06bfc78549d0dba52d32b2a852225092
woollace <97724026+woollace@users.noreply.github.com>
2025-04-24 20:34:52 +0800

    Update OfficerMainView.java

--- src/views/OfficerMainView.java
+++ src/views/OfficerMainView.java
@@ -15,7 +15,7 @@ public class OfficerMainView implements IDisplayResult {
         System.out.println("5. Apply As Project Officer Registrations");
         System.out.println("6. Reply Managed Project Enquiries");
         System.out.println("7. Manage Project Bookings");
-        System.out.println("8. Manage Application Bookings");
+        System.out.println("8. Generate Application Bookings Receipt");
         System.out.println("9. Change Password");
         System.out.println("10. Logout");
         System.out.print("\nEnter option: ");
